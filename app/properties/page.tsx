2745211a04812737fbde6537a8311e7f8d9a84bc
Juhasz Bence <juhbence90@gmail.com>
2026-01-21 11:10:26 +0100

    feat(properties): create properties page and cards, properties.json added

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,5 +1,25 @@
+import PropertyCard from '@/components/PropertyCard';
+import properties from '@/properties.json';
+import type { Property } from '@/app/types/property';
+
+const propertiesData: Property[] = properties;
+
 const PropertiesPage = () => {
-  return <div>Properties Page</div>;
+  return (
+    <section className="px-4 py-6">
+      <div className=" lg:container m-auto px-4 py-6">
+        {propertiesData.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {propertiesData.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default PropertiesPage;
