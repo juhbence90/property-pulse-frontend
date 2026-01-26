bba480e38ce35f88c636cd6d5ba67133d198d363
Juhasz Bence <juhbence90@gmail.com>
2026-01-26 21:13:57 +0100

    feat: add properties form component created

--- app/properties/add/page.tsx
+++ app/properties/add/page.tsx
@@ -1,5 +1,15 @@
+import PropertyAddForm from '@/components/PropertyAddForm';
+
 const AddPropertyPage = () => {
-  return <div>Add Property Page</div>;
+  return (
+    <section className="bg-blue-50">
+      <div className="container m-auto max-w-xl py-24">
+        <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md  m-4 md:m-0">
+          <PropertyAddForm />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default AddPropertyPage;
