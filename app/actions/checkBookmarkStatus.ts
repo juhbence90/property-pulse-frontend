3c128b4b65741ac2812b2b1a649d59206fc990bc
Juhasz Bence <juhbence90@gmail.com>
2026-02-02 17:37:50 +0100

    feat: bookmark and saved properties page

---
+++ app/actions/checkBookmarkStatus.ts
@@ -0,0 +1,31 @@
+'use server';
+import connectDB from '@/config/database';
+import User from '@/models/User';
+import { getSessionUser } from '@/utils/getSessionUser';
+
+type CheckBookmarkStatusResponse = {
+  isBookmarked: boolean;
+  error?: string;
+};
+
+async function checkBookmarkStatus(
+  propertyId: string,
+): Promise<CheckBookmarkStatusResponse> {
+  await connectDB;
+
+  const sessionUser = await getSessionUser();
+
+  if (!sessionUser || !sessionUser.userId) {
+    throw new Error('User ID is required');
+  }
+
+  const { userId } = sessionUser;
+
+  const user = await User.findById(userId);
+
+  const isBookmarked = user.bookmarks.includes(propertyId);
+
+  return { isBookmarked };
+}
+
+export default checkBookmarkStatus;
--- components/BookmarkButton.tsx
+++ components/BookmarkButton.tsx
@@ -1,9 +1,74 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import bookmarkProperty from '@/app/actions/bookmarkProperty';
+import checkBookmarkStatus from '@/app/actions/checkBookmarkStatus';
+import { toast } from 'react-toastify';
 import type { PropertyProps } from '@/app/types/property';
 import { FaBookmark } from 'react-icons/fa';
+import { useSession } from 'next-auth/react';
 
 const BookmarkButton = ({ property }: PropertyProps) => {
-  return (
-    <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
+  const { data: session } = useSession();
+  const userId = session?.user?.id;
+
+  const [isBookmarked, setIsBookmarked] = useState(false);
+
+  useEffect(() => {
+    if (!userId) {
+      return;
+    }
+
+    const fetchBookmarkStatus = async () => {
+      try {
+        const res = await checkBookmarkStatus(property._id);
+
+        if (res.error) toast.error(res.error);
+        if (res.isBookmarked) setIsBookmarked(res.isBookmarked);
+      } catch (error) {
+        if (error instanceof Error) {
+          toast.error(error.message);
+        } else {
+          toast.error('Something went wrong.');
+        }
+      }
+    };
+
+    fetchBookmarkStatus();
+  }, [property._id, userId]);
+
+  const handleClick = async () => {
+    if (!userId) {
+      toast.error('You need to be signed in to bookmark a listing');
+      return;
+    }
+
+    try {
+      const res = await bookmarkProperty(property._id);
+
+      setIsBookmarked(res.isBookmarked);
+      toast.success(res.message);
+    } catch (error) {
+      if (error instanceof Error) {
+        toast.error(error.message);
+      } else {
+        toast.error('Something went wrong.');
+      }
+    }
+  };
+
+  return isBookmarked ? (
+    <button
+      className="bg-green-500 hover:bg-green-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+      onClick={handleClick}
+    >
+      <FaBookmark className="mr-2" /> Bookmarked
+    </button>
+  ) : (
+    <button
+      className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+      onClick={handleClick}
+    >
       <FaBookmark className="mr-2" /> Bookmark Property
     </button>
   );
